39222f0c511159844f606fa348734c92c12d9edd
pawelwilga <pawel@wilga..net.pl>
2025-03-25 18:55:59 +0100

    Poprawiona konfiguracja

--- constants/statusCode.js
+++ constants/statusCode.js
@@ -1,3 +1,12 @@
 // 🔧 Zdefiniuj obiekt STATUS_CODE, który przechowuje kody statusu HTTP 302 (FOUND) oraz 404 (NOT_FOUND).
 
 // 🔧 Wyeksportuj STATUS_CODE, który przechowuje kody statusu HTTP.
+
+const STATUS_CODES = {
+    FOUND: 302,
+    NOT_FOUND: 404
+};
+
+module.exports = {
+    STATUS_CODES
+}
